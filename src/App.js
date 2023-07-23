c35fedaa2c621e6dec0710458a3ba54fe3e6dc84
Chandru-Ghub <chandruinfo455@gmail.com>
2023-07-24 00:36:36 +0530

    renewed

--- src/App.js
+++ src/App.js
@@ -2,73 +2,83 @@ import './App.css';
 import { useState } from 'react';
 import Content from './Content';
 import Main from './Main';
-import Filtertask from './Filtertask';
 import Footer from './Footer';
 import Option from './Option'
 
 function App() {
   const [task, setTask] = useState('');
 
-const options = [
-  {value: 'All', text: 'All'},
-  {value: 'Completed', text: 'Completed'},
-  {value: 'Not Completed', text: 'Not Completed'},
- 
-];
 
-const [selected, setSelected] = useState(options[0].value);
-
-const handleChange = event => {
-  // console.log(event.target.value);
-  setSelected(event.target.value);
-};
-
-
-
+const[value,setValue] = useState()
+const[text,setText] = useState(true)
+const[indata,setIndata] = useState(0)
 function del(id){
     let rem = task.filter((a)=>(a.id!==id))
     setTask(rem);
     
 }
-
+function edit(id){
+    let editData = task.find((a)=>a.name==id)
+    setItems(editData.name)
+    setItems2(editData.description)
+    setText(false)
+    console.log(id)
+    setIndata(id)
+}
 const [newItems,setItems]=useState('');
 const [newItems2,setItems2]=useState('');
 const handleSubmit=(e)=>{
   e.preventDefault();
   
-  if(!newItems ) return;
-  addItems(newItems,newItems2);
-  setItems('');
-  setItems2('');
+    if(indata){
+      let res = task.find((a)=>a.name==indata)
+      console.log(res)
+      let update = task.map((a)=>a.name==res.name?
+      ({name:newItems,description:newItems2})
+      :({name:a.name,description:a.description})
+      )
+      console.log(update)
+      setTask(update)
+      setItems('')
+      setItems2('')
+     setText(true)
+     setIndata(0)
+    
+    }
+
+    else if(newItems != '',newItems2 != ''){
+
+      const add = [...task,{name:newItems,description:newItems2}]
+      setTask(add)
+      setItems('')
+      setItems2('')
+    }
+    
+
+   
 }
-const addItems=(statu,desc)=>{
-  const id = task.length? task[task.length-1].id+1:1
-  const addNewItem ={id,name:statu,description:desc,status:'Not completed'}
-  const listItem = [...task,addNewItem]
-  setTask(listItem)
-}
+
 
 
   return (
     <div className='body'>
+      {/* <p>welcome{indata}</p> */}
       <Main
       handleSubmit={handleSubmit}
       setItems = {setItems}
       newItems = {newItems}
       setItems2 = {setItems2}
       newItems2 = {newItems2}
+      text = {text}
+      setText = {setText}
       />
-      <Option
-      handleChange = {handleChange}
-      selected = {selected}
-      setSelected = {setSelected}
-      options={options}
-      />
+      <Option />
 
      
       <Content
       task = {task}
       del = {del}
+      edit={edit}
       />
       <Footer
       length = {task.length}/>
